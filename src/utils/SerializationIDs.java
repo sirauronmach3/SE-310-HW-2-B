71a262ef8fc9cf3e0f6c385803a3bdd19a505c1c
@mjm836 <mjm836@drexel.edu>
2024-05-22 21:02:44 -0400

    Added enums to SerializationIDs

--- src/utils/SerializationIDs.java
+++ src/utils/SerializationIDs.java
@@ -1,4 +1,22 @@
 package utils;
 
 public enum SerializationIDs {
+    SURVEY(1L),
+    QUESTION(2L),
+    ESSAY(3L),
+    MULTIPLE_CHOICE(5L),
+    SHORT_ANSWER(6L),
+    TRUE_FALSE(7L),
+    VALID_DATE(8L),
+    RESPONSE(9L),
+    DATE_RESPONSE(11L),
+    OPEN_ENDED_RESPONSE(12L),
+    PAIR_RESPONSE(13L),
+    SELECTION_RESPONSE(14L),
+    SHORT_ANSWER_RESPONSE(15L);
+
+    public final long id;
+    SerializationIDs(long id) {
+        this.id = id;
+    }
 }
