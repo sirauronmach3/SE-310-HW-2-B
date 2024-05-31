b3d565cb12143d863b70c1855abb0c6835a7d386
@mjm836 <mjm836@drexel.edu>
2024-05-31 16:52:07 -0400

    Added test for isEqual

--- test/PairResponseTest.java
+++ test/PairResponseTest.java
@@ -1,2 +1,25 @@
+import Survey.Response.PairResponse;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.HashMap;
+
 public class PairResponseTest {
+    private PairResponse pair1;
+
+    @Test
+    public void PairResponseIsEqualsTrueIfSame() {
+        PairResponse pair2 = new PairResponse();
+        pair1 = new PairResponse();
+
+        HashMap<String, String> response1 = new HashMap<>();
+        HashMap<String, String> response2 = new HashMap<>();
+        String left = "left";
+        String right = "right";
+        response1.put(left, right);
+        response2.put(left, right);
+
+        assertTrue(pair1.isEqual(pair2));
+    }
 }
