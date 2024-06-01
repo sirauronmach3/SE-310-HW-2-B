64e566c2c827c284bc028db415da9f1ecd39ca9d
mjm836 <mjm836>
2024-05-31 20:37:20 -0400

    refactored first isEqual test, added additional test

--- test/PairResponseTest.java
+++ test/PairResponseTest.java
@@ -19,7 +19,32 @@ public class PairResponseTest {
         String right = "right";
         response1.put(left, right);
         response2.put(left, right);
+        pair1.setAnswer(response1);
+        pair2.setAnswer(response2);
 
         assertTrue(pair1.isEqual(pair2));
+        assertTrue(pair2.isEqual(pair1));
+    }
+
+    @Test
+    public void PairResponseIsEqualsFalseIfDifferent() {
+        PairResponse pair2 = new PairResponse();
+        pair1 = new PairResponse();
+
+        HashMap<String, String> response1 = new HashMap<>();
+        HashMap<String, String> response2 = new HashMap<>();
+        String left1 = "left";
+        String left2 = "LEFT";
+        String right1 = "right";
+        String right2 = "RIGHT";
+        response1.put(left1, right1);
+        response1.put(left2, right2);
+        response2.put(left1, right2);
+        response2.put(left2, right1);
+
+        pair1.setAnswer(response1);
+        pair2.setAnswer(response2);
+
+        assertFalse(pair1.isEqual(pair2));
     }
 }
