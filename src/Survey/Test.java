1e8a7ff1bfeab48f145eb2a6146afab9d34d068e
@mjm836 <mjm836@drexel.edu>
2024-06-01 10:42:15 -0400

    Created Test

--- src/Survey/Test.java
+++ src/Survey/Test.java
@@ -1,4 +1,10 @@
 package Survey;
 
+import utils.SerializationIDs;
+
 public class Test {
+    /**
+     * ID for serialization
+     */
+    private final static long serialVersionUID = SerializationIDs.TEST.uid;
 }
