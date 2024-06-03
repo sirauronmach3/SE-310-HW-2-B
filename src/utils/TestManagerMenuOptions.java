e54d1bf8bd4f0fe07952cdee055532f6c643d9e4
mjm836 <mjm836>
2024-06-03 19:25:22 -0400

    Populated enum

--- src/utils/TestManagerMenuOptions.java
+++ src/utils/TestManagerMenuOptions.java
@@ -1,4 +1,20 @@
 package utils;
 
 public enum TestManagerMenuOptions {
+    CREATE_NEW("Create a new Test"),
+    DISPLAY_WITH_KEY("Display an existing Test with correct Answers"),
+    DISPLAY_WITHOUT_KEY("Display an existing Test without correct Answers"),
+    LOAD("Load an existing Test"),
+    SAVE("Save the current Test"),
+    TAKE("Take the current Test"),
+    MODIFY("Modify the current Test"),
+    TABULATE("Tabulate the current Test"),
+    GRADE("Grade a Test"),
+    RETURN("Return to the previous menu");
+
+    public final String name;
+    TestManagerMenuOptions(String name) {
+        this.name = name;
+    }
+
 }
