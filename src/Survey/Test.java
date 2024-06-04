dc17979fb944a58866437464272591787720b0b0
@mjm836 <mjm836@drexel.edu>
2024-06-04 07:09:17 -0400

    Added members

--- src/Survey/Test.java
+++ src/Survey/Test.java
@@ -1,10 +1,15 @@
 package Survey;
 
+import Survey.Response.Response;
 import utils.SerializationIDs;
 
-public class Test {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Test extends Survey{
     /**
      * ID for serialization
      */
     private final static long serialVersionUID = SerializationIDs.TEST.uid;
+    private final ArrayList<Response> correctAnswers = new ArrayList<>();
 }
