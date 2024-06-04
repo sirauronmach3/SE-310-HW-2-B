cc41de33d61b7bff00cd5ee046029826aac79e34
@mjm836 <mjm836@drexel.edu>
2024-06-04 06:11:01 -0400

    Implemented displayMenu and handleMenuSelection. Still need all calling methods.

--- src/Management/TestManager.java
+++ src/Management/TestManager.java
@@ -25,8 +25,61 @@ public class TestManager extends SurveyManager{
             out.say("Test Menu Options:");
             Out.getInstance().say("-------------------------------");
             for (int i = 0; i < size; i++) {
-                out.say((i + 1) + ": " + TestManagerMenuOptions.values()[i].toString());
+                out.say((i + 1) + ": " + TestManagerMenuOptions.values()[i].name);
+            }
+            try {
+                out.say("Pick a menu option by number");
+                selection = in.readIntWithinRange(1, size);
+            } catch (Exception e) {
+                selection = 0;
+            } finally {
+                cont = handleMenuSelection(TestManagerMenuOptions.values()[(selection - 1)]);
             }
         }
+    }
+
+    protected boolean handleMenuSelection(TestManagerMenuOptions selection) {
+        Out out = Out.getInstance();
+        switch (selection) {
+            case CREATE_NEW:
+                out.say("Selection: " + TestManagerMenuOptions.CREATE_NEW.name);
+                createTest();
+                break;
+            case DISPLAY_WITH_KEY:
+                out.say("Selection: " + TestManagerMenuOptions.DISPLAY_WITH_KEY.name);
+                break;
+            case DISPLAY_WITHOUT_KEY:
+                out.say("Selection: " + TestManagerMenuOptions.DISPLAY_WITHOUT_KEY.name);
+                break;
+            case LOAD:
+                out.say("Selection: " + TestManagerMenuOptions.LOAD.name);
+                break;
+            case SAVE:
+                out.say("Selection: " + TestManagerMenuOptions.SAVE.name);
+                break;
+            case TAKE:
+                out.say("Selection: " + TestManagerMenuOptions.TAKE.name);
+                break;
+            case MODIFY:
+                out.say("Selection: " + TestManagerMenuOptions.MODIFY.name);
+                break;
+            case TABULATE:
+                out.say("Selection: " + TestManagerMenuOptions.TABULATE.name);
+                break;
+            case GRADE:
+                out.say("Selection: " + TestManagerMenuOptions.GRADE.name);
+                break;
+            case RETURN:
+                out.say("Selection: " + TestManagerMenuOptions.RETURN.name);
+                return quit();
+            default:
+                out.say("Invalid Selection");
+                break;
+        }
+        return true;
+    }
+
+    private void createTest() {
+
     }
 }
