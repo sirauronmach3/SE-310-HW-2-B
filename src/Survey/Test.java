3aee47dc82600a58599617afaf20c163aeda04c7
cyrilmartinmj <BhAmPNA7GBk8nKy>
2024-06-05 12:39:01 -0400

    Implemented displayWithKey

--- src/Survey/Test.java
+++ src/Survey/Test.java
@@ -1,9 +1,11 @@
 package Survey;
 
+import Management.QuestionType;
+import Survey.Question.Question;
 import Survey.Response.Response;
+import utils.Out;
 import utils.SerializationIDs;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Test extends Survey{
@@ -24,6 +26,33 @@ public class Test extends Survey{
 
     @Override
     public void displayWithKey() {
+        Out out = Out.getInstance();
+        int questionNumber = 0;
+        Question question;
+        Response answer;
 
+        out.say("The Test with Correct Answers");
+        out.say("---------------------------");
+
+        for (int i = 0; i < questions.size(); i++) {
+            // setup
+            questionNumber++;
+            question = questions.get(i);
+            answer = correctAnswers.get(i);
+
+            // display question and possibly given answer
+            out.say("Question #" + questionNumber + " " + question.getQuestionType().label + " question: ");
+            question.display();
+
+            // display correct answer
+            out.say("");
+            if (question.getQuestionType() == QuestionType.ESSAY) {
+                out.say("No specifically correct answer for this question.");
+            } else {
+                out.say("Correct answer for question #" + questionNumber +":");
+                answer.display();
+            }
+
+        }
     }
 }
