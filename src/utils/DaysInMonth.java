d371cbaf9436ea7f2a67ed75a9fd5f7df0c9ad88
@mjm836 <mjm836@drexel.edu>
2024-06-08 14:07:50 -0400

    Added member variable to store days in February during leap year, no magic numbers.

--- src/utils/DaysInMonth.java
+++ src/utils/DaysInMonth.java
@@ -2,4 +2,5 @@ package utils;
 
 public class DaysInMonth {
     public static final int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static final int FEBRUARY_LEAP_YEAR = 29;
 }
